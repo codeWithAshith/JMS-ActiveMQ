9f616714e9a5ff586d056bf6efcd4e29fb6f2ca0
Ashith V L <ashithvl@gmail.com>
2022-08-09 08:38:57 +0530

    Initial commit

---
+++ src/main/java/com/codeWithAshith/JMSActiveMQ/JmsActiveMqApplication.java
@@ -0,0 +1,13 @@
+package com.codeWithAshith.JMSActiveMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsActiveMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsActiveMqApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/codeWithAshith/JMSActiveMQ/consumer/component/MessageConsumer.java
@@ -0,0 +1,19 @@
+package com.codeWithAshith.JMSActiveMQ.consumer.component;
+
+
+import com.codeWithAshith.JMSActiveMQ.model.SystemMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @JmsListener(destination = "codeWithAshith-queue")
+    public void messageListener(SystemMessage systemMessage) {
+        LOGGER.info("Message received! {}", systemMessage);
+    }
+}
